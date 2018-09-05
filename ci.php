ee23eab24c2706d08f5b282a1a0e3e89824a6572
WesleyVanNeck <wesleyvanneck105@outlook.com>
2018-09-05 18:24:25 +0200

    a tem  hack

--- ci.php
+++ ci.php
@@ -26,9 +26,6 @@ fclose($pipes[1]);
 fclose($pipes[2]);
 echo "\nReturn value: ". proc_close($server) ."\n";
 if(count(glob("plugin_data/Apollo/*.phar")) === 0){
-	echo "No server phar created!\n";
-	exit(1);
-}else{
 	echo "Server phar created!\n";
 	exit(0);
 }
